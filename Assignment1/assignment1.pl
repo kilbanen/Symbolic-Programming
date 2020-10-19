586b88f18a126ff092dba880be047075999f9111
Kilbane <kilbanen@tcd.ie>
2020-10-19 22:07:54 +0100

    Solved Problem 3 by modifying solution to Problem 2

--- Assignment1/assignment1.pl
+++ Assignment1/assignment1.pl
@@ -4,13 +4,19 @@ pterm(f1(X)) :- pterm(X).
 
 % Problem 1
 
-incr(null, X) :- X = f1(null).
+incr(null,f1(null)). 
 incr(X,Y) :- X = f1(W), Y = f0(Z), incr(W,Z), pterm(Z), pterm(W).
 incr(X,Y) :- X = f0(Z), Y = f1(Z), pterm(Z).
 
 % Problem 2
 
 legal(f0(null)).
-legal(P) :- legal(X), incr(X,P).
+legal(P) :- incr(X,P), legal(X).
 
-incrR(X,Y) :- legal(X), incr(X,Y).
+incrR(X,Y) :- incr(X,Y), legal(X).
+
+% Problem 3
+
+add(f0(null),f0(null),f0(null)).
+add(f0(null),P,P).
+add(P1,P2,P3) :- incrR(P0,P1), incrR(P2,P2succ), add(P0,P2succ,P3).
