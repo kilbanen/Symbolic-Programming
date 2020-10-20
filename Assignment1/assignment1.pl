2bffab204e51593e62b16995d29f51044dd9fb5c
Kilbane <kilbanen@tcd.ie>
2020-10-20 19:22:21 +0100

    Solved Problem 5 after wracking my brain for half a day

--- Assignment1/assignment1.pl
+++ Assignment1/assignment1.pl
@@ -24,3 +24,11 @@ add(P1,P2,P3) :- incrR(P0,P1), incrR(P2,P2succ), add(P0,P2succ,P3).
 
 mult(f0(null),P,f0(null)).
 mult(P1,P2,P3) :- incrR(X,P1), mult(X,P2,Y), add(P2,Y,P3).
+
+% Problem 5
+
+revcon(null,X,X).
+revcon(X,Y,Z) :- X = f0(W), revcon(W,f0(Y),Z).
+revcon(X,Y,Z) :- X = f1(W), revcon(W,f1(Y),Z).
+
+revers(P,RevP) :- revcon(P,null,RevP).
